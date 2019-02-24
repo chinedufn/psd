15721c6287bba79f4311060848d9cd1ae5c83a8c
ci-build <ci-build@klukas.net>
2019-02-24 21:17:32 +0000

    [skip ci] Updates

---
+++ api/psd/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["ColorMode","Adobe Docs"],["PsdChannelCompression","How is this layer channel data compressed?"],["PsdChannelKind","The different kinds of channels in a layer (red, green, blue, ...)."]],"struct":[["Psd","Represents the contents of a PSD file"],["PsdLayer","Information about a layer in a PSD file."]]});
