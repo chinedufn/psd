1389ad3d513fa23cc53738cdb85b0790c670f296
Lucas Czaplinski <lukasz.czaplinski@ceros.com>
2022-07-17 19:27:39 +0200

    retract boundary check
    it was part of my testing branch and looks like I made a mistake whilst
    cherry-picking. will come back with some tests for this case :)

--- src/psd_channel.rs
+++ src/psd_channel.rs
@@ -159,10 +159,7 @@ pub trait IntoRgba {
                 let bytes_to_read = 1 + header;
                 for byte in cursor.read(bytes_to_read as u32) {
                     let rgba_idx = self.rgba_idx(idx);
-                    let target = rgba_idx * 4 + offset;
-                    if target < rgba.len() {
-                        rgba[target] = *byte;
-                    }
+                    rgba[rgba_idx * 4 + offset] = *byte;
 
                     idx += 1;
                 }
@@ -171,10 +168,7 @@ pub trait IntoRgba {
                 let byte = cursor.read_1()[0];
                 for _ in 0..repeat as usize {
                     let rgba_idx = self.rgba_idx(idx);
-                    let target = rgba_idx * 4 + offset;
-                    if target < rgba.len() {
-                        rgba[target] = byte;
-                    }
+                    rgba[rgba_idx * 4 + offset] = byte;
 
                     idx += 1;
                 }
