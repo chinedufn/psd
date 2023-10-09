c3fe8030341850ef1c1a0f00bc7f2ec18a5e5c74
Firelight Flagboy <firelight.flagboy@gmail.com>
2023-10-09 13:46:46 +0200

    Fix compilation error about missing `dyn` in cast

--- examples/drag-drop-browser/src/lib.rs
+++ examples/drag-drop-browser/src/lib.rs
@@ -53,7 +53,7 @@ impl AppWrapper {
 
                     store.borrow_mut().msg(&Msg::SetIsRendering(false));
                 };
-                let mut re_render = Closure::wrap(Box::new(re_render) as Box<FnMut()>);
+                let mut re_render = Closure::wrap(Box::new(re_render) as Box<dyn FnMut()>);
 
                 window().request_animation_frame(&re_render.as_ref().unchecked_ref());
 
