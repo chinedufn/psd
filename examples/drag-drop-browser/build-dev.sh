3ae670051ddd5d0dedbb98713a85b4ea18758b6e
Jacob Dallas <10dallasj@gmail.com>
2024-11-11 07:44:35 -0600

    BUG FIX update wasm-pack in circleci build step

--- examples/drag-drop-browser/build-dev.sh
+++ examples/drag-drop-browser/build-dev.sh
@@ -7,5 +7,5 @@ cd "$(dirname "$0")"
 mkdir -p public
 
 CSS_FILE="$(pwd)/public/app.css"
-OUTPUT_CSS=$CSS_FILE wasm-pack build --no-typescript --dev --target web --out-dir ./public
+OUTPUT_CSS=$CSS_FILE wasm-pack build --no-typescript --dev --target web --out-dir ./public --no-opt
 cp index.html public/
